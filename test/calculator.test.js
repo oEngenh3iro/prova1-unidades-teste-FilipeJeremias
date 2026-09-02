eb4835a5b0d90f8af59244c0473a15672fb6e9ef
oEngenh3iro <filipejeremis692@gmail.com>
2026-09-01 22:33:22 -0300

    corrigindo teste - CI verde novamente

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,7 @@
 const { somar, diminuir, multiplicar, dividir } = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
-  expect(somar(150, 25)).toStrictEqual(999);
+  expect(somar(150, 25)).toStrictEqual(175);
 });
 
 test("Diminuir dois valores válidos", async () => {
